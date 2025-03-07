9009895215c7626695cdab75ac9541fca13801d6
sdd13 <sdd13@student.le.ac.uk>
2025-03-07 10:13:44 +0000

    Dashboard Left Panel Completed

--- Code/frontend/src/Component/Dashboard/Dashboard.jsx
+++ Code/frontend/src/Component/Dashboard/Dashboard.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import {
   Container,
   Row,
-  Column,
+  Col,
   Card,
   Button,
   Navbar,
   Nav,
 } from "react-bootstrap";
-import { FaUser, FaSignOutAlt } from "react-icons/fa";
+import {
+  FaUser,
+  FaSignOutAlt,
+  FaCalendarAlt,
+  FaPrescriptionBottleAlt,
+  FaFileAlt,
+} from "react-icons/fa";
 
 export default function Dashboard() {
   return (
@@ -66,6 +72,83 @@ export default function Dashboard() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* {Left Side Navbar} */}
+      {/* {https://getbootstrap.com/docs/4.4/utilities/sizing/} */}
+      <Container fluid>
+        <Row>
+          <Col
+            className="h-100 d-inline-block"
+            md={3}
+            lg={2}
+            variant="dark"
+            style={{
+              backgroundColor: "#041D4A",
+              position: "fixed",
+              paddingTop: "60px",
+              bottom: 0,
+              // height: "cal(100vh - 56px)", https://stackoverflow.com/questions/52941346/css-height-calc100vh-vs-height-100vh
+            }}
+          >
+            <Nav className="flex-column" style={{ height: "100%" }}>
+              <Nav.Link
+                className="mt-4 mb-2 "
+                href="#Appointment Bookings"
+                style={{ color: "white" }}
+              >
+                <FaCalendarAlt
+                  style={{ marginRight: "6px", marginBottom: "2px" }}
+                />
+                Appointment Bookings
+              </Nav.Link>
+
+              <Nav.Link
+                className="mt-4 mb-2 "
+                href="#Prescriptions"
+                style={{ color: "white" }}
+              >
+                <FaPrescriptionBottleAlt style={{ marginRight: "6px" }} />
+                Prescriptions
+              </Nav.Link>
+
+              <Nav.Link
+                className="mt-4 mb-2 "
+                href="#Medical Records"
+                style={{ color: "white" }}
+              >
+                <FaFileAlt style={{ marginRight: "6px" }} />
+                Medical Records
+              </Nav.Link>
+
+              <Nav.Link
+                className="mt-4 mb-2 "
+                href="#Profile"
+                style={{ color: "white" }}
+              >
+                <FaUser style={{ marginRight: "6px" }} />
+                Profile
+              </Nav.Link>
+
+              <div
+                className="d-flex align-items-end mb-4 "
+                style={{ height: "100%" }}
+              >
+                <Nav.Link
+                  className="mt-auto "
+                  href="#Logout"
+                  style={{
+                    marginLeft: "-150px",
+                    color: "white",
+                  }}
+                >
+                  <FaSignOutAlt style={{ marginRight: "6px" }} />
+                  Logout
+                </Nav.Link>
+              </div>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
