ef8668ebc088d43489bbdde358282f10fc46d0a0
sdd13 <sdd13@student.le.ac.uk>
2025-03-29 20:56:17 +0000

    Updated Tables

---
+++ SQLScripts/Updated Tables.sql
@@ -0,0 +1,55 @@
+Create database GP;
+
+Use GP;
+
+CREATE TABLE Admin (
+    Admin_id INT PRIMARY KEY IDENTITY(1,1),
+    Admin_Password VARCHAR(255) NOT NULL,
+    Admin_Email VARCHAR(255) UNIQUE,
+
+);
+
+--  Patient table
+CREATE TABLE Patient (
+    P_id INT IDENTITY(1,1) PRIMARY KEY,
+    P_FirstName VARCHAR(255) NOT NULL,
+	P_LastName VARCHAR(255) NOT NULL,
+    Gender VARCHAR(50) NOT NULL,
+	DOB DATE NOT NULL,
+    Email_Id VARCHAR(255) NOT NULL UNIQUE,
+    Phone_No VARCHAR(15) NOT NULL UNIQUE,
+    PatientPassword VARCHAR(255) NOT NULL,
+	Registered_By_Admin INT FOREIGN KEY REFERENCES Admin(Admin_id)
+);
+
+
+CREATE TABLE Doctor (
+    Doctor_id INT PRIMARY KEY IDENTITY(1,1),
+    Doctor_FirstName VARCHAR(255),
+    Doctor_LastName VARCHAR(255),
+    Doctor_Email VARCHAR(255) UNIQUE,
+    Doctor_Phone_No VARCHAR(15) UNIQUE,
+    Doctor_Registration_Number VARCHAR(15) UNIQUE,
+    Specialization VARCHAR(255),
+    D_Password VARCHAR(255) NOT NULL,
+	Registered_By_Admin INT FOREIGN KEY REFERENCES Admin(Admin_id)
+);
+
+CREATE TABLE Nurse (
+    Nurse_id INT PRIMARY KEY IDENTITY(1,1),
+    Nurse_FirstName VARCHAR(255),
+    Nurse_LastName VARCHAR(255),
+    Nurse_Email VARCHAR(255) UNIQUE,
+    Nurse_Phone_No VARCHAR(15) UNIQUE,
+    Nurse_Registration_Number VARCHAR(15) UNIQUE,
+    Specialization VARCHAR(255),  -- e.g., "Diabetes Care", "Wound Care"
+    N_Password VARCHAR(255)	NOT NULL,
+	Registered_By_Admin INT FOREIGN KEY REFERENCES Admin(Admin_id)
+);
+
+
+
+DELETE FROM Admin where Admin_Email= 'Uol.admin@healme.com'
+Select * from Admin
+
+USE GP;
